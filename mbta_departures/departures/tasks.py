5e004d7ad1cabec638353f3978f35b3b1fe9ad02
Malcolm <mbalchcr@oberlin.edu>
2016-08-23 16:51:07 -0400

    download and parse departures csv

--- mbta_departures/departures/tasks.py
+++ mbta_departures/departures/tasks.py
@@ -1,8 +1,22 @@
 
+import requests
+import String.IO
+import csv
+
+from utils import update_departures
+
 from celery.decorators import task, periodic_task
 from celery.task.schedules import crontab
 
 @periodic_task(run_every=(crontab()), name="demo_task", ignore_result=True)
-def update_departures():
-    pass
-    # TODO: call departures API and handle database entry 
+def fetch_departure_data():
+    res = requests.get("http://developer.mbta.com/lib/gtrtfs/Departures.csv")
+    if res.status_code == 200:
+        f = StringIO.StringIO(res.content)
+        reader = csv.reader(f, delimiter=',')
+
+        # Truncate first row (contains field definitions)
+        rows = list(reader)[1:]
+        update_departures(rows)
+    else:
+        pass
---
+++ mbta_departures/departures/utils.py
@@ -0,0 +1,4 @@
+from models import Departure
+
+def update_departures(departures):
+    pass
