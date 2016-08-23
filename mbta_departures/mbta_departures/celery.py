4c61d034259e1ae3f45d86acd55e5fede0705cfa
Malcolm <mbalchcr@oberlin.edu>
2016-08-23 16:59:46 -0400

    remove celery testing tasks

--- mbta_departures/mbta_departures/celery.py
+++ mbta_departures/mbta_departures/celery.py
@@ -12,11 +12,3 @@ app = Celery('mbta_departures')
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
-
-@task(name="sum_two_numbers")
-def add(x, y):
-    return x + y
