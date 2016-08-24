d5ab1a03838ac9846a4bd1f4b60b913ed3afda7f
Malcolm <mbalchcr@oberlin.edu>
2016-08-24 10:43:20 -0400

    include jquery, bind on pageload

--- mbta_departures/departures/views.py
+++ mbta_departures/departures/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.http import HttpResponse
@@ -9,12 +11,22 @@ class DepartureListView(ListView):
     model = Departure 
     template_name = 'departures/departures_list.html'
 
-    def get_queryset(self):
-        # TODO: How long to keep "departed" trains around for? 
-        return Departure.objects.all()
-
-    '''
     def get_context_data(self, **kwargs):
         context = super(DepartureListView, self).get_context_data(**kwargs)
+        
+        # TODO: When is a departure no longer needed?
+        current_departures = Departure.objects.all()
+
+        json_departures = []
+        for dep in current_departures:
+            json_departures.append({
+                'origin':'Hell',
+                'trip':'',
+                'destination':'',
+                'scheduled_time':'',
+                'lateness':'',
+                'track':'',
+                'status':'',
+            })
+        context['json_departures'] = json.dumps(json_departures)
         return context
-    '''
