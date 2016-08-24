134b2e1417fa7c084b1132bc41c66a049067c9ac
Malcolm <mbalchcr@oberlin.edu>
2016-08-23 20:00:15 -0400

    listview for departures

--- mbta_departures/departures/views.py
+++ mbta_departures/departures/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 from django.http import HttpResponse
 
-def index(request):
-    # TODO: How long to keep "departed" trains around for? 
-    return HttpResponse("Hello, world. You're at the departures index.")
+from models import Departure
+
+class DepartureListView(ListView):
+
+    model = Departure 
+    template_name = 'departures/departures_list.html'
+
+    def get_queryset(self):
+        # TODO: How long to keep "departed" trains around for? 
+        return Departure.objects.all()
+
+    '''
+    def get_context_data(self, **kwargs):
+        context = super(DepartureListView, self).get_context_data(**kwargs)
+        return context
+    '''
