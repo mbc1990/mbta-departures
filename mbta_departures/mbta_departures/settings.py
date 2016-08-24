1c7e5481342910d9c086852d2334f2db1ecc09d1
Malcolm <malcolmbalch@gmail.com>
2016-08-24 12:19:29 -0400

    static root

--- mbta_departures/mbta_departures/settings.py
+++ mbta_departures/mbta_departures/settings.py
@@ -101,6 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # CELERY STUFF
 BROKER_URL = 'redis://localhost:6379'
